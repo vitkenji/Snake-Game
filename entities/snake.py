b0c985b810537c9a12175945004ecafb5f4d12ae
vitkenji <vkz.yamada@gmail.com>
2025-05-05 21:30:09 -0300

    inputManager instance in snake

--- entities/snake.py
+++ entities/snake.py
@@ -1,58 +1,43 @@
 import pygame as pg
 import random
 import constants as c
+from managers.input_manager import InputManager 
 
 class Snake:
     def __init__(self):
         self.length = 1
-        self.head_position = [c.TILE_SIZE*random.randint(0, 31), c.TILE_SIZE*random.randint(0, 23)]
-        self.last_direction = (1, 0)
+        self.head_position = [
+            c.TILE_SIZE * random.randint(0, 31),
+            c.TILE_SIZE * random.randint(0, 23)
+        ]
         self.body = []
         self.score = 0
-        
+        self.input_manager = InputManager()
+
     def get_direction(self) -> tuple:
-        keys = pg.key.get_pressed()
-        
-        if keys[pg.K_w] and self.last_direction != (0, 1):
-            self.last_direction = (0, -1)
-        
-        elif keys[pg.K_a] and self.last_direction != (1, 0):
-            self.last_direction = (-1, 0)
-        
-        elif keys[pg.K_s] and self.last_direction != (0, -1):
-            self.last_direction = (0, 1)
-        
-        elif keys[pg.K_d] and self.last_direction != (-1, 0):
-            self.last_direction = (1, 0)
-        
-        return self.last_direction
-    
+        self.input_manager.update_direction()
+        return self.input_manager.get_direction()
+
     def check_wall_collision(self):
-        if self.head_position[0] < 0 or self.head_position[0] > c.WIDTH:
+        if self.head_position[0] < 0 or self.head_position[0] >= c.WIDTH:
             return True
-        elif self.head_position[1] < 0 or self.head_position[1] > c.HEIGHT:
+        elif self.head_position[1] < 0 or self.head_position[1] >= c.HEIGHT:
             return True
         return False
 
     def check_snake_collision(self):
-        for segment in self.body:
-            if segment == self.head_position:
-                return True
-        return False
+        return self.head_position in self.body
 
     def check_apple_collision(self, apple_position, direction):
-
-        next_head_position = [self.head_position[0] + direction[0]*25, self.head_position[1] + direction[1]*25] 
+        next_head_position = [
+            self.head_position[0] + direction[0] * c.TILE_SIZE,
+            self.head_position[1] + direction[1] * c.TILE_SIZE
+        ]
         if next_head_position == apple_position:
-            self.body.insert(0, self.head_position)
-            self.head_position = apple_position
+            self.body.insert(0, list(self.head_position))
+            self.head_position = list(apple_position)
             return True
-
         return False
-    
+
     def restart(self):
-        self.length = 1
-        self.head_position = [c.TILE_SIZE*random.randint(0, 31), c.TILE_SIZE*random.randint(0, 23)]
-        self.last_direction = (1, 0)
-        self.body = []
-        self.score = 0
+        self.__init__()
--- managers/input_manager.py
+++ managers/input_manager.py
@@ -7,17 +7,17 @@ class InputManager:
     def update_direction(self):
         keys = pg.key.get_pressed()
 
-        if keys[pg.K_w] and self.last_direction != (0, 1):
-            self.last_direction = (0, -1)
+        if keys[pg.K_w] and self.direction != (0, 1):
+            self.direction = (0, -1)
         
-        elif keys[pg.K_a] and self.last_direction != (1, 0):
-            self.last_direction = (-1, 0)
+        elif keys[pg.K_a] and self.direction != (1, 0):
+            self.direction = (-1, 0)
         
-        elif keys[pg.K_s] and self.last_direction != (0, -1):
-            self.last_direction = (0, 1)
+        elif keys[pg.K_s] and self.direction != (0, -1):
+            self.direction = (0, 1)
         
-        elif keys[pg.K_d] and self.last_direction != (-1, 0):
-            self.last_direction = (1, 0)
+        elif keys[pg.K_d] and self.direction != (-1, 0):
+            self.direction = (1, 0)
     
     def get_direction(self):
         return self.direction
